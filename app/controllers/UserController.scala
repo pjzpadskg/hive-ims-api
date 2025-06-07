db1086582a42d021510effab15d0a513622b65fc
PJ Santos <pauloletics@gmail.com>
2025-06-08 06:16:58 +0800

    Update UserController.scala

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,13 +1,26 @@
 package controllers
 
-import javax.inject._
-import play.api._
+import javax.inject.{ Singleton, Inject }
+import scala.concurrent.{ Future, ExecutionContext }
+import play.api.i18n.I18nSupport
+import play.api.libs.json.Json
 import play.api.mvc._
+
+import dtos.UserCreate
+import models.service.UserService
 
 @Singleton
 class UserController @Inject()(
-  val controllerComponents: ControllerComponents
-) extends BaseController {
-  def create = TODO
-  def get = TODO
+  val controllerComponents: ControllerComponents,
+  UserService: UserService
+)(using ExecutionContext) extends BaseController with I18nSupport {
+  def create = Action.async { implicit request: Request[AnyContent] =>
+    UserCreate.form.bindFromRequest().fold(
+      error => Future.successful(BadRequest(error.errorsAsJson)),
+      data => UserService.create(data).fold(
+        err => Status(err.code)(Json.obj("error" -> err.reason)),
+        _ => Created
+      )
+    )
+  }
 }
