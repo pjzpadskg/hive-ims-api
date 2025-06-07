9a5767d2ba312300cb7bf6b3431eb9a95a03fbd9
PJ Santos <pauloletics@gmail.com>
2025-06-08 06:16:38 +0800

    Add Error handler

---
+++ app/errors/Error.scala
@@ -0,0 +1,14 @@
+package errors
+
+case class Error(code: Int, reason: String)
+
+object Error {
+  val BAD_REQUEST = 400
+  val UNAUTHORIZED = 401
+  val FORBIDDEN = 403
+  val NOT_FOUND = 404
+  val CONFLICT = 409
+  val INTERNAL = 500
+
+  val USER_ALREADY_EXISTS = "USER_ALREADY_EXISTS"
+}
