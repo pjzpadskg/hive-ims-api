8b4584d7ecf2c72f14b3a4a3c59cc47f7437eec1
PJ Santos <pauloletics@gmail.com>
2025-06-08 08:11:05 +0800

    Update SessionController.scala

--- app/controllers/SessionController.scala
+++ app/controllers/SessionController.scala
@@ -1,12 +1,29 @@
 package controllers
 
-import javax.inject._
-import play.api._
+import javax.inject.{ Singleton, Inject }
+import scala.concurrent.{ Future, ExecutionContext }
+import play.api.i18n.I18nSupport
+import play.api.libs.json.Json
 import play.api.mvc._
+
+import dtos.UserCredentials
+import models.service.UserService
 
 @Singleton
 class SessionController @Inject()(
-  val controllerComponents: ControllerComponents
-) extends BaseController {
-  def index() = Action { implicit request: Request[AnyContent] => Ok }
+  val controllerComponents: ControllerComponents,
+  UserService: UserService
+)(using ExecutionContext) extends BaseController with I18nSupport {
+
+  def create = Action.async { implicit request: Request[AnyContent] => 
+    UserCredentials.form.bindFromRequest().fold(
+      error => Future.successful(BadRequest(error.errorsAsJson)),
+      credentials => UserService.get(credentials).fold(
+        _ => Unauthorized,
+        user => Ok.withSession("sessionKey" -> user.id.toString)
+      )
+    )
+  }
+
+  def delete = Action { implicit request: Request[AnyContent] => Ok.withNewSession }
 }
